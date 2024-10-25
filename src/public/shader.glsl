27d5acf46fc96d170504e923826ec4acbbf0484e
Bjørn Einar Bjartnes <bjorn.einar.bjartnes@4subsea.com>
2024-10-25 12:52:35 +0200

    ok

--- src/public/shader.glsl
+++ src/public/shader.glsl
@@ -4,18 +4,32 @@ uniform vec3 iResolution;
 uniform float iTime;
 varying vec2 vUv;
 
+float smoothMin(float a, float b, float k) {
+    float h = max(k - abs(a - b), 0.0);
+    return min(a, b) - h * h * 0.25 / k;
+}
+
+// https://iquilezles.org/articles/distfunctions/
+float sdRoundBox( vec3 p, vec3 b, float r )
+{
+  vec3 q = abs(p) - b + r;
+  return length(max(q,0.0)) + min(max(q.x,max(q.y,q.z)),0.0) - r;
+}
+
+
 float distance_from_sphere(in vec3 p, in vec3 c, float r) {
-  return min(
-    length(p - c)- r,
-    length(p-(c+vec3(-1.8,0.2,3.1)))-r
-  );
+  return min(min( length(p - c)- r,
+                    length(p-(c+vec3(2.0*sin(iTime*10.0),0.2,3.1)))-r),
+            sdRoundBox(p-vec3(1.0*3.0*sin(iTime),1.0,5.0), vec3(1.0, 0.5, 2.0), 0.1));
 }
 
 float map_the_world(in vec3 p) {
-  float displacement = sin(1.0 * p.x*iTime) * sin(2.0 * p.y*1.0*iTime) * tan(1.0 * p.z) * 0.95;
+  float pi = 3.1415926538;
+  //float foo = mod(1.0*iTime,pi*2.0);
+  float foo = sin(1.0*iTime);
+  float displacement = 0.3*sin(foo)  * 0.15;
   float sphere_0 = distance_from_sphere(p, vec3(0.0), 1.0);
-
-  return sphere_0 + displacement;
+  return sphere_0 + 0.3*displacement;
 }
 
 vec3 calculate_normal(in vec3 p) {
@@ -31,13 +45,13 @@ vec3 calculate_normal(in vec3 p) {
 }
 
 
-vec3 light_position = vec3(0.0, 0.0, -10.0);
-vec3 camera_position = vec3(0.0, 1.0, -4.0);
+vec3 light_position = vec3(15.0, -5.0, -10.0);
+vec3 camera_position = vec3(-1.0, 2.0, -6.0);
 
 vec3 ray_march(in vec3 ro, in vec3 ray_direction) {
   float total_distance_traveled = 0.0;
   const int NUMBER_OF_STEPS = 32;
-  const float MINIMUM_HIT_DISTANCE = 0.001;
+  const float MINIMUM_HIT_DISTANCE = 0.00001;
   const float MAXIMUM_TRACE_DISTANCE = 1000.0;
 
   for (int i = 0; i < NUMBER_OF_STEPS; ++i)
@@ -53,9 +67,10 @@ vec3 ray_march(in vec3 ro, in vec3 ray_direction) {
       //vec3 direction_to_light = normalize(current_position - light_position);
       vec3 direction_to_light = normalize(light_position - current_position);
 
-      float diffuse_intensity = max(0.1, 2.0*dot(normal, direction_to_light));
+      float diffuse_intensity = max(0.2, 0.8*dot(normal, direction_to_light));
 
-      return (current_position.yzx+ 0.1)  * diffuse_intensity;
+      //return vec3(0.5)*diffuse_intensity;
+      return ((current_position.xyx)+ 0.1)  * diffuse_intensity;
     }
 
     if (total_distance_traveled > MAXIMUM_TRACE_DISTANCE) {
@@ -68,7 +83,7 @@ vec3 ray_march(in vec3 ro, in vec3 ray_direction) {
 }
 
 void main() {
-  camera_position += iTime*0.01; 
+//  camera_position += iTime*0.01; 
   vec2 uv = vUv.st * 2.0 - 1.0;
 
   vec3 ray_origin = camera_position;
