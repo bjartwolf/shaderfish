4e3c184396d9bf473676e9cb0bd23ba34f992482
Bjørn Einar Bjartnes <bjorn.einar.bjartnes@4subsea.com>
2024-10-25 20:08:08 +0200

    try vite now

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,16 +1,22 @@
-import { defineConfig } from 'vite'
 import FullReload from 'vite-plugin-full-reload'
-
+import { resolve } from 'path'
 /** @type {import('vite').UserConfig} */
 export default {
   plugins: [
-    FullReload(['**/*'])
+    FullReload(['**/*']),
   ],
   assetsInclude: ['**/*.png', '**/*.glb', '**/*.glsl'],
   build: {
-    target: 'esnext'
+    target: 'esnext',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        task1: resolve(__dirname, 'task1/index.html'),
+        task2: resolve(__dirname, 'task2/index.html'),
+        task3: resolve(__dirname, 'task3/index.html'),
+      }
+    }
   }
-  // ...
 }
 
 
